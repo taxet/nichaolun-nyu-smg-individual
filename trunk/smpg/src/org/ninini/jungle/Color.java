f1c7fd00a025910ed52815ecd9cd3d22027ada33
taxetBHS@gmail.com <taxetBHS@gmail.com@96624485-e097-2442-667a-85f386a22377>
2013-09-27 21:35:21 +0000

    implement gwt p2

---
+++ trunk/smpg/src/org/ninini/jungle/Color.java
@@ -0,0 +1,21 @@
+package org.ninini.jungle;
+
+public enum Color {
+
+	RED,
+	BLACK,
+	;
+	
+	public boolean isBlack(){
+		return this == BLACK;
+	}
+	
+	public Color getOpposite(){
+		return this == RED ? BLACK : RED;
+	}
+	
+	@Override
+	public String toString(){
+		return isBlack()?"B":"R";
+	}
+}
---
+++ trunk/smpg/src/org/ninini/jungle/IllegalMove.java
@@ -0,0 +1,11 @@
+package org.ninini.jungle;
+
+public class IllegalMove extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public IllegalMove(){
+		
+	}
+
+}
